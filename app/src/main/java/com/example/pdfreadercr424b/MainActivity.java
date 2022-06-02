44f9045049ea12194f81962776952f35b6c6e0e4
nhattien015 <tienit0105@gmail.com>
2022-06-02 15:29:17 +0700

    feat: add toolbar actions

--- app/src/main/java/com/example/pdfreadercr424b/MainActivity.java
+++ app/src/main/java/com/example/pdfreadercr424b/MainActivity.java
@@ -1,36 +1,118 @@
 package com.example.pdfreadercr424b;
 
 import android.Manifest;
+import android.app.ListActivity;
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Environment;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+
 import java.io.File;
+import java.text.Normalizer;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.zip.Inflater;
+
+import kotlin.text.Regex;
+
+import com.example.pdfreadercr424b.R;
 
 public class MainActivity extends AppCompatActivity {
     ListView pdfListView;
     public static ArrayList<File> fileList = new ArrayList<File>();
-    PDFAdapter obj_adapter;
+    PDFAdapter pdfAdapter;
     public static int REQUEST_PERMISSIONS = 1;
     boolean boolean_permission;
     File dir;
+    private Toolbar toolbar;
+
+    public String removeAccents(String inputStr){
+        return Normalizer.normalize(inputStr, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = null;
+        if (searchItem != null) {
+            searchView = (SearchView) searchItem.getActionView();
+        }
+        if (searchView != null) {
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(MainActivity.this.getComponentName()));
+        }
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+
+
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                Log.i("Kei", removeAccents(newText));
+                Predicate<File> filterSearch = file -> (removeAccents(file.getName().toLowerCase())).contains(removeAccents(newText.toLowerCase()));
+
+
+                pdfAdapter = new PDFAdapter(getApplicationContext(), (ArrayList<File>)fileList.stream().filter(filterSearch).collect(Collectors.toList()));
+                pdfListView.setAdapter(pdfAdapter);
+                return true;
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.open_specific_:
+        }
+
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+
 
     }
 
@@ -39,8 +121,20 @@ public class MainActivity extends AppCompatActivity {
         pdfListView = (ListView) findViewById(R.id.pdfList);
         dir = new File(Environment.getExternalStorageDirectory().toString());
         //  dir = new File(String.valueOf(Environment.getExternalStorageDirectory()));
-        fn_permission();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                initPermissions();
+            }
+        }).start();
 
+        findViewById(R.id.scan_btn).setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                initPermissions();
+            }
+        });
         pdfListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -50,26 +144,30 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
     }
 
     public ArrayList<File> getfile(File dir) {
-        File allFiles[] = dir.listFiles();
-        if (allFiles != null && allFiles.length > 0) {
-            for (int i = 0; i < allFiles.length; i++) {
+        Set<File> allFiles = new HashSet<File>();
+        allFiles.addAll(Arrays.asList(dir.listFiles()));
+        File[] files = allFiles.toArray(new File[0]);
+        if (allFiles != null && allFiles.size() > 0) {
+            for (int i = 0; i < allFiles.size(); i++) {
 
-                if (allFiles[i].isDirectory()) {
-                    getfile(allFiles[i]);
+                if (files[i].isDirectory()) {
+                    getfile(files[i]);
 
                 } else {
-                    if (allFiles[i].getName().endsWith(".pdf") && !fileList.contains(allFiles[i])) {
-                        fileList.add(allFiles[i]);
+
+                    if (files[i].getName().endsWith(".pdf") || files[i].getName().endsWith(".epub") && !fileList.contains(files[i])) {
+                        fileList.add(files[i]);
                     }
                 }
             }
         }
         return fileList;
     }
-    private void fn_permission() {
+    private void initPermissions() {
         if ((ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)) {
 
             if ((ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, android.Manifest.permission.READ_EXTERNAL_STORAGE))) {
@@ -82,9 +180,8 @@ public class MainActivity extends AppCompatActivity {
             boolean_permission = true;
 
             getfile(dir);
-
-            obj_adapter = new PDFAdapter(getApplicationContext(), fileList);
-            pdfListView.setAdapter(obj_adapter);
+            pdfAdapter = new PDFAdapter(getApplicationContext(), fileList);
+            pdfListView.setAdapter(pdfAdapter);
 
         }
     }
@@ -92,21 +189,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == REQUEST_PERMISSIONS) {
 
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (requestCode == REQUEST_PERMISSIONS) {
 
-                boolean_permission = true;
-                getfile(dir);
+               if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-                obj_adapter = new PDFAdapter(getApplicationContext(), fileList);
-                pdfListView.setAdapter(obj_adapter);
+                    boolean_permission = true;
+                    getfile(dir);
 
-            } else {
-                Toast.makeText(getApplicationContext(), "Please allow the permission", Toast.LENGTH_LONG).show();
+                   pdfAdapter = new PDFAdapter(getApplicationContext(), fileList);
+                    if(pdfAdapter.pdfList.size() < 0){
+                        findViewById(R.id.emptyView).setVisibility(View.VISIBLE);
+                    }
+                    else{
+                        findViewById(R.id.emptyView).setVisibility(View.INVISIBLE);
+                    }
+                    pdfListView.setAdapter(pdfAdapter);
 
+                } else {
+                    Toast.makeText(getApplicationContext(), "Please allow the permission", Toast.LENGTH_LONG).show();
+
+                }
             }
-        }
+
     }
 
 }
